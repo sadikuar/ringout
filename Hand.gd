d964256b1e067b7ea25f98e27c7c2258b8a7a68e
Artan Sadiku <artan.sadiku@he-arc.ch>
2023-02-08 17:25:29 +0100

    - Changed collision to be active only when punching

--- Hand.gd
+++ Hand.gd
@@ -4,26 +4,23 @@ var disable_movements = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	# TODO: Need to access disabled movements to stop this hand from punching
+	$CollisionShape2D.disabled = true
 	var player = get_parent()
 	
 	if player != null:
 		disable_movements = player.disable_movements
-	
-	$CollisionShape2D.disabled = true
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not disable_movements:
 		if Input.is_action_pressed("Punch 1"):
+			$CollisionShape2D.disabled = false
 			$AnimatedSprite.play()
 
 
 func _on_AnimatedSprite_animation_finished():
-	$CollisionShape2D.disabled = false
+	$CollisionShape2D.disabled = true
 	$AnimatedSprite.stop()
 	$AnimatedSprite.frame = 0
 
