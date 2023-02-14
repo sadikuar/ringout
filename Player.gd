22d7edf6a30668059149e19aaf665da1df0782ab
Artan Sadiku <artan.sadiku@he-arc.ch>
2023-02-14 18:05:17 +0100

    Create basic sprite sheet
    Added 3 basic sprite sheets to the game for:
    - Idle
    - Running
    - Punching
    
    Moved hand code inside the player script

--- Player.gd
+++ Player.gd
@@ -7,35 +7,83 @@ export var disable_movements = false
 
 var screen_size = null
 
+var punching = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Hand/HandHitBox.disabled = true
+	
 	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not disable_movements:
-		var velocity = Vector2.ZERO
-		if Input.is_action_pressed("Move right"):
-			velocity.x += 1
-		if Input.is_action_pressed("Move left"):
-			velocity.x -= 1
-		if Input.is_action_pressed("Move up"):
-			velocity.y -= 1
-		if Input.is_action_pressed("Move down"):
-			velocity.y += 1
+		if Input.is_action_just_pressed("Punch 1"):
+			punching = true
+			$PlayerSprite.speed_scale = 2
+			$PlayerSprite.play("punching")
+			$Hand/HandHitBox.disabled = false
 		
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
+		if not punching:
+			var velocity = Vector2.ZERO
+			if Input.is_action_pressed("Move right"):
+				velocity.x += 1
+			if Input.is_action_pressed("Move left"):
+				velocity.x -= 1
+			if Input.is_action_pressed("Move up"):
+				velocity.y -= 1
+			if Input.is_action_pressed("Move down"):
+				velocity.y += 1
+				
+			if velocity.x < 0:
+				$PlayerSprite.flip_h = true
+				if $PlayerHitBox.position.x < 0:
+					flip_collision_shapes()
+			elif velocity.x > 0:
+				if $PlayerHitBox.position.x > 0:
+					flip_collision_shapes()
+
+				$PlayerSprite.flip_h = false
+			
+			if velocity.length() > 0:
+				$PlayerSprite.speed_scale = 2
+				$PlayerSprite.play("running")
+				velocity = velocity.normalized() * speed
+			else:
+				$PlayerSprite.speed_scale = 0.5
+				$PlayerSprite.play("idle")
 	
-		move_and_collide(velocity * delta)
+			move_and_collide(velocity * delta)
 		
 		# Binds player to the viewport's size
-		position.x = clamp(position.x, $PlayerSprite.get_rect().size.x / 2, screen_size.x - $PlayerSprite.get_rect().size.x / 2)
-		position.y = clamp(position.y, $PlayerSprite.get_rect().size.y / 2, screen_size.y - $PlayerSprite.get_rect().size.y / 2)
+		position.x = clamp(position.x, $PlayerHitBox.shape.extents.x, screen_size.x - $PlayerHitBox.shape.extents.x)
+		position.y = clamp(position.y, $PlayerHitBox.shape.extents.y, screen_size.y - $PlayerHitBox.shape.extents.y)
 
 func start_hit_countdown():
 	$HitCountdown.start()
 	
 func get_time_left():
 	return $HitCountdown.get_time_left()
+	
+func flip_collision_shapes():
+	$PlayerHitBox.position.x *= -1
+	$Hand.scale.x *= -1
+
+
+func _on_Hand_body_entered(body):
+		# Hit countdown has not started or has finished
+	if body.get_time_left() == 0:
+		body.position.x += 10 # Apply position change to player
+		body.health -= 10 # Reduce player health
+		body.start_hit_countdown() # Start countdown again
+	
+	if body.health == 0:
+		body.queue_free()
+
+
+func _on_PlayerSprite_animation_finished():
+	if $PlayerSprite.animation == "punching":
+		$PlayerSprite.frame = 0
+		$Hand/HandHitBox.disabled = true
+		punching = false
